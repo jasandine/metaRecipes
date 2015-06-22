85718724d8799690c8818088afb933ab44489d7f
jasandine <jasandine@gmail.com>
2015-06-22 09:42:35 -0700

    Update Gemfile, add rating gem

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'devise'
 
 gem 'paperclip'
 
+gem 'ratyrate'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ActiveRecord::Base
   has_attached_file :image, styles: { large: "200x200" }
   belongs_to :user
+  validates_attachment_size :image, :less_than => 5.megabytes
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
